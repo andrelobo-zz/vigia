78f5755e1f5010b0e5f2c5ca2f42c56ac97cb4cf
andrelobo <loboandre@hotmail.com>
2021-04-19 15:47:37 -0400

    Initial commits

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    URL: process.env.URL || "http://localhost:3333",
+    MONGO_DB_URI: 'mongodb+srv://Nhacapimba:Hope70x07@cluster0.yydip.mongodb.net/Esfomeado?retryWrites=true&w=majority' || 'mongodb://localhost:27017/kanbant'
+}
